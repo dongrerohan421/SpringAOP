be025b0447ff06bafc075b2eaa60a506f6ae2472
Rohan Dongre <dongrerohan421@gmail.com>
2016-05-16 15:59:58 -0500

    create simple spring project
    create simple spring project

---
+++ src/main/java/com/spring/aop/AopMain.java
@@ -0,0 +1,28 @@
+package com.spring.aop;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.spring.aop.service.ShapeService;
+
+public class AopMain {
+
+	public static void main(String[] args) {
+
+		try {
+
+			ApplicationContext context = new ClassPathXmlApplicationContext("springAOP.xml");
+
+			// Used "ShapeService.class" to type cast it automatically according
+			// to class
+			ShapeService shapeService = context.getBean("shapeService", ShapeService.class);
+			System.out.println(shapeService.getCircle().getName());
+			System.out.println(shapeService.getTriangle().getName());
+
+		} catch (Exception e) {
+
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
