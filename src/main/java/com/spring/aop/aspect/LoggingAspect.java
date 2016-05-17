675e8fb5a3569caa04a137f0dd2db0903274d360
Rohan Dongre <dongrerohan421@gmail.com>
2016-05-16 20:47:14 -0500

    add Pointcuts and Wildcard Expressions
    add Pointcuts and Wildcard Expressions

--- src/main/java/com/spring/aop/aspect/LoggingAspect.java
+++ src/main/java/com/spring/aop/aspect/LoggingAspect.java
@@ -2,13 +2,26 @@ package com.spring.aop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 
 	// executes LoggingAdvice() before execution of getName()
-	@Before("execution(public String getName())")
+	// @Before("execution(public String getName())")
+	@Before("allGetters()")
 	public void LoggingAdvice() {
 		System.out.println("Advice run.Get method called");
+	}
+
+	@Before("allGetters()")
+	public void secondAdvice() {
+		System.out.println("Second advice executed.");
+	}
+
+	// Pointcut – Indicate which method should be intercept, by method name or
+	// regular expression pattern.
+	@Pointcut("execution(* get*())")
+	public void allGetters() {
 	}
 }
